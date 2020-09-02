4bb4ab9ae6c28f85dc77d63dfc27b9a8ab54ae11
3-24 <dev.youngseok@gmail.com>
2020-09-03 00:49:39 +0900

    first session

---
+++ session01/hello1.scala
@@ -0,0 +1,4 @@
+/* Execute it by typing 'scala hello1.scala' */
+/* The compliation step takes some time */
+println("Hello World")
+println("Hello CS320")
---
+++ session01/hello2.scala
@@ -0,0 +1,9 @@
+/* Type 'scalac hello2.scala' to compile the program.
+It generates Hello.class and Hello$.class files.
+To launch the program, type 'scala Hello'. */
+object Hello {
+    def main(args: Array[String]): Unit = {
+        println("Hello World")
+        println("Hello CS320")
+    }
+}
